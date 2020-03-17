cf150bde93d9e5950da5e2d8eb596b8808d5e92a
diego <d.riosrojas007@gmail.com>
2020-03-17 00:52:32 -0300

    cambiar imagen usuario listo

---
+++ app/components/Account/InfoUser.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Avatar } from "react-native-elements";
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+
+export default function InfoUser(props) {
+  /* extrae los datos del usuario logeado del props*/
+  const {
+    userInfo: { uid, displayName, email, photoURL },
+    setReloadData,
+    toastRef,
+    setIsLoading,
+    setTextIsLoading
+  } = props;
+
+  /* boton modificar imagen */
+  const changeAvatar = async () => {
+    const resultPermission = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+    const resultPermissionCamera =
+      resultPermission.permissions.cameraRoll.status;
+
+    /* pregunta si se acepto el permiso de la camara */
+    if (resultPermissionCamera === "denied") {
+      toastRef.current.show("Es necesario aceptar lo permisos de la galeria");
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3]
+      });
+
+      /* si se selecciono una imagen o no */
+      if (result.cancelled) {
+        toastRef.current.show("has cerrado la galeria de imagenes");
+      } else {
+        uploadImage(result.uri, uid).then(() => {
+          updatePhotoUrl(uid);
+        });
+      }
+    }
+  };
+
+  /* funcion para subir imagen a firebase storage */
+  const uploadImage = async (uri, nameImage) => {
+    setTextIsLoading("Actualizando Avatar");
+    setIsLoading(true);
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const ref = firebase
+      .storage()
+      .ref()
+      .child(`avatar/${nameImage}`);
+    return ref.put(blob);
+  };
+
+  /* funcion para actualizar imagen del usuario  */
+  const updatePhotoUrl = uid => {
+    firebase
+      .storage()
+      .ref(`avatar/${uid}`)
+      .getDownloadURL()
+      .then(async result => {
+        const update = {
+          photoURL: result
+        };
+        await firebase.auth().currentUser.updateProfile(update);
+        setReloadData(true);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        toastRef.current.show("Error al recuperar el avatar del servidor");
+      });
+  };
+
+  return (
+    <View style={styles.viewUserInfo}>
+      <Avatar
+        rounded
+        size="large"
+        showEditButton
+        onEditPress={changeAvatar}
+        containerStyle={styles.userInfoAvatar}
+        source={{
+          uri: photoURL
+            ? photoURL
+            : "https://api.adorable.io/avatars/264/abott@adorable"
+        }}
+      />
+      <View>
+        <Text style={styles.displayName}>
+          {displayName ? displayName : "Anónimo"}
+        </Text>
+        <Text>{email ? email : "Social Login"}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewUserInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f2f2f2",
+    paddingTop: 30,
+    paddingBottom: 30
+  },
+  userInfoAvatar: {
+    marginRight: 20
+  },
+  displayName: {
+    fontWeight: "bold"
+  }
+});
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, Text } from "react-native";
 import { Button } from "react-native-elements";
 import * as firebase from "firebase";
+import InfoUser from "../../components/Account/InfoUser";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/Loading";
 
 export default function UserLogged() {
+  /* extrae los datos del usuario logeado */
+  const [userInfo, setUserInfo] = useState({});
+  /* recargar imagen usuario al actualizarla */
+  const [reloadData, setReloadData] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [textLoading, setTextIsLoading] = useState("");
+
+  const toastRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const user = await firebase.auth().currentUser;
+      setUserInfo(user.providerData[0]);
+    })();
+    setReloadData(false);
+  }, [reloadData]);
+
   return (
     <View>
-      <Text>UserLogged...</Text>
+      <InfoUser
+        userInfo={userInfo}
+        setReloadData={setReloadData}
+        toastRef={toastRef}
+        setIsLoading={setIsLoading}
+        setTextIsLoading={setTextIsLoading}
+      />
       <Button title="cerrar sesion" onPress={() => firebase.auth().signOut()} />
+      <Toast ref={toastRef} position="center" opacity={0.5} />
+      <Loading text={textLoading} isVisible={isLoading} />
     </View>
   );
 }
